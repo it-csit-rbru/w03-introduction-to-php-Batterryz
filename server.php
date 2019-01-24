6a251248f9ed9a9eadc52d246cefcd68a29a4956
Batterryz <46773497+Batterryz@users.noreply.github.com>
2019-01-24 16:21:50 +0700

    Add files via upload

---
+++ server.php
@@ -0,0 +1,24 @@
+#6014421002 Chanthimatorn Chanthawong 
+<?php
+    $email = "6014421002@rbru.ac.th";
+    $name = "Batterryz";
+    $url = "stu2.rbru.ac.th/~s6014421002";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Batterrzzzzzz</title>
+</head>
+<body>
+    <?php
+        echo $_SERVER["HTTP_USER_AGENT"],"<br>";
+        echo $_SERVER["HTTP_HOST"],"<br>";
+        echo $_SERVER["SERVER_ADDR"],"<br>"
+    ?>    
+</body>
+</html>
+
+localhost/php-id-w03/server_info.php
